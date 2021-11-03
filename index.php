fb4085a6e1c5d2b763c721ffbec6d4004883d03b
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2021-11-03 20:35:18 +0200

    update meta-tag

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="main.css" rel="stylesheet" type="text/css">
         <script src="slideshow.js"></script>
     </head>
