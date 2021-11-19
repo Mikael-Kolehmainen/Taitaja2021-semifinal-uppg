54a090de04d4245876c317ccf389dfe12f3a4693
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2021-11-19 21:20:31 +0200

    Update 19.11.2021 finally got items.js to work properly

--- items.js
+++ items.js
@@ -1,24 +1,30 @@
 function setItems(images, titles, qty, prices) {
-    window.localStorage.setItem('images', images);
+    window.localStorage.setItem('images', JSON.stringify(images));
     window.localStorage.setItem('titles', titles);
     window.localStorage.setItem('qty', qty);
     window.localStorage.setItem('prices', prices);
 }
 function showItems() {
-    itemImages = window.localStorage.getItem('images')
+    itemImages = JSON.parse(window.localStorage.getItem('images'))
     itemTitles = window.localStorage.getItem('titles')
     itemQty = window.localStorage.getItem('qty')
     itemPrices = window.localStorage.getItem('prices')
 
+    var shopItem = document.createElement('tr')
+    shopItem.className = 'shop-item'
+    var row = document.createElement('td')
+
     if (itemImages.length > 0) {
         for (i = 0; 1 >= i; i++) {
-            // document.write
-            document.getElementById('orderedItems').appendChild(document.createTextNode("<tr class=shop-item"))
-            for (j = 1; j <= 2; j++) {
-                document.append("<td>")
+            document.getElementById('orderedItems').appendChild(shopItem)
+            for (j = 0; j < itemImages.length; j++) {
+                var shopItemImage = document.createElement('img')
+                shopItemImage.className = 'shop-item-image'
+                shopItem.appendChild(row)
                 switch(i) {
                     case 1:
-                        document.append("<img src=" + itemImages[j] + " class='shop-item-image'>")
+                        shopItemImage.src = itemImages[j]
+                        row.appendChild(shopItemImage)
                         break;
                     case 2:
                         document.append("<div id='product' class='shop-item-title'>" + itemTitles[j] + "</div>")
@@ -26,15 +32,9 @@ function showItems() {
                         document.append("Kpl: " + itemQty[j])
                         break;
                 }
-                document.append("</td>")
             }
-            document.append("</tr>")
         }
     } else {
         document.append("Empty")
     }
-    console.log(itemImages)
-    console.log(itemTitles)
-    console.log(itemQty)
-    console.log(itemPrices)
 }
