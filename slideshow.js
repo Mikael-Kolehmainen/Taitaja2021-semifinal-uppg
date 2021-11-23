ae63b862886db786665c2817a3f64da44396d324
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2021-11-23 14:42:52 +0200

    Fixed bug in slideshow,js

--- slideshow.js
+++ slideshow.js
@@ -1,40 +1,32 @@
-if (document.readyState == 'loading') {
-  document.addEventListener('DOMContentLoaded', ready)
-} else {
-  ready()
-}
+var slideIndex = 1;
+var timer = null;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+showSlides(slideIndex);
+currentSlide(1);
 
-function ready() {
-  var slideIndex = 1;
-  var timer = null;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  showSlides(slideIndex);
-  currentSlide(1);
-
-  function plusSlides(n) {
+function plusSlides(n) {
   clearTimeout(timer);
   showSlides(slideIndex += n);
-  }
+}
 
-  function currentSlide(n) {
+function currentSlide(n) {
   clearTimeout(timer);
   showSlides(slideIndex = n);
-  }
+}
 
-  function showSlides(n) {
-  var i;
-  if (n==undefined){n = ++slideIndex}
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  timer = setTimeout(showSlides, 4000);
-  }
+function showSlides(n) {
+var i;
+if (n==undefined){n = ++slideIndex}
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+timer = setTimeout(showSlides, 4000);
 }
